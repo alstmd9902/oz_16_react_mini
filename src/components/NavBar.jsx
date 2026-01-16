23ad9c1db526449fa03a6b0c5e578413248bf378
Seungyeon Oh <alstmd9902@naver.com>
2026-01-16 14:20:00 +0900

    refactor: 버튼 스타일 위치 조정

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,7 @@
-export default function NavBar() {
-  const buttonStyle = "bg-violet-600 px-3 py-1 rounded-md text-white";
+//렌더링 될시 한번만 스타일 적용
+const buttonStyle = "bg-violet-600 px-3 py-1 rounded-md text-white";
 
+export default function NavBar() {
   return (
     <nav className="h-14 bg-black w-full flex items-center">
       <div className="max-w-7xl mx-auto w-full flex items-center gap-4 justify-between px-6">
